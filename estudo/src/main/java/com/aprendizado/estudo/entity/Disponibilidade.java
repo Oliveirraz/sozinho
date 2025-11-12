0a95a8352fd4923bee1e66c3b09cf26997bec5e7
Glêisson de Oliveira Bezerra <oliveirabezerraeu@gmail.com>
2025-11-12 14:53:57 -0300

    1.2

--- estudo/src/main/java/com/aprendizado/estudo/entity/Disponibilidade.java
+++ estudo/src/main/java/com/aprendizado/estudo/entity/Disponibilidade.java
@@ -2,6 +2,8 @@ package com.aprendizado.estudo.entity;
 
 import jakarta.persistence.*;
 
+import java.time.LocalTime;
+
 @Entity
 @Table(name = "disponibilidade")
 public class Disponibilidade {
@@ -9,4 +11,17 @@ public class Disponibilidade {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(nullable = false, name = "dia_da_semana")
+    @Enumerated(EnumType.STRING)
+    private DiaEnum diaDaSemana;
+
+    private LocalTime horaDisponivel;
+
+
+
+    public enum DiaEnum{
+            SEGUNDA, TERCA, QUARTA,
+        QUINTA, SEXTA;
+    }
 }
